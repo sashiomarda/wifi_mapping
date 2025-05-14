5e18fc24328bef555ec49cd9ac2119bc3ca1ed89
Jihand Aulia Sashiomarda <sashiomarda@gmail.com>
2025-05-14 10:35:32 +0700

    feat: add loading image and broken image into 3D image after uploading all 2D images to the server

--- app/src/main/java/com/sashiomarda/wifimapping/ui/downloadMap/DownloadMapScreen.kt
+++ app/src/main/java/com/sashiomarda/wifimapping/ui/downloadMap/DownloadMapScreen.kt
@@ -18,6 +18,7 @@ import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -28,6 +29,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -80,6 +83,8 @@ import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.core.content.ContextCompat.checkSelfPermission
 import com.sashiomarda.wifimapping.components.ImageFromFile
@@ -194,7 +199,7 @@ fun DownloadMapScreen(
         }
     }
 
-    var isUploading2DImages by remember { mutableStateOf(false) }
+    var isUploading2DImages by remember { mutableStateOf(true) }
     var resultUploading2DImages by remember { mutableStateOf<String?>(null) }
 
     Scaffold(
@@ -354,76 +359,118 @@ fun DownloadMapScreen(
                                 modifier = Modifier
                                     .padding(bottom = 8.dp)
                             ) {
-                                Row(
-                                    modifier = Modifier
-                                        .fillMaxWidth()
-                                        .padding(8.dp)
-                                        .clickable {
-                                            isButton3DExpandClicked = !isButton3DExpandClicked
-                                            if (Build.VERSION.SDK_INT < 34) {
-                                                when {
-                                                    checkSelfPermission(
-                                                        context,
-                                                        Manifest.permission.WRITE_EXTERNAL_STORAGE
-                                                    ) == PackageManager.PERMISSION_GRANTED -> {
-                                                        isPermissionGranted.value = true
-                                                    }
+                                Column {
+                                    Row(
+                                        modifier = Modifier
+                                            .fillMaxWidth()
+                                            .padding(8.dp)
+                                            .clickable {
+                                                isButton3DExpandClicked = !isButton3DExpandClicked
+                                                if (Build.VERSION.SDK_INT < 34) {
+                                                    when {
+                                                        checkSelfPermission(
+                                                            context,
+                                                            Manifest.permission.WRITE_EXTERNAL_STORAGE
+                                                        ) == PackageManager.PERMISSION_GRANTED -> {
+                                                            isPermissionGranted.value = true
+                                                        }
 
-                                                    else -> {
-                                                        permissionLauncher.launch(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                                                        else -> {
+                                                            permissionLauncher.launch(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                                                        }
                                                     }
-                                                }
-                                                if (isPermissionGranted.value) {
+                                                    if (isPermissionGranted.value) {
+                                                        if (allImageFileListDb.isNotEmpty()) {
+                                                            val imageParts =
+                                                                getImageParts(allImageFileListDb = allImageFileListDb)
+                                                            CoroutineScope(Dispatchers.IO).launch {
+                                                                try {
+                                                                    val response =
+                                                                        RetrofitClient.instance.uploadImages(
+                                                                            imageParts
+                                                                        )
+                                                                    withContext(Dispatchers.Main) {
+                                                                        isUploading2DImages = false
+                                                                        resultUploading2DImages =
+                                                                            if (response.isSuccessful) "Upload berhasil" else "Upload gagal: ${response.code()}"
+                                                                    }
+                                                                } catch (e: Exception) {
+                                                                    withContext(Dispatchers.Main) {
+                                                                        isUploading2DImages = false
+                                                                        resultUploading2DImages =
+                                                                            "Error: ${e.localizedMessage}"
+                                                                    }
+                                                                }
+                                                            }
+                                                        }
+                                                    }
+                                                } else {
                                                     if (allImageFileListDb.isNotEmpty()) {
-                                                        val imageParts = getImageParts(allImageFileListDb = allImageFileListDb)
+                                                        val imageParts =
+                                                            getImageParts(allImageFileListDb = allImageFileListDb)
                                                         CoroutineScope(Dispatchers.IO).launch {
                                                             try {
-                                                                val response = RetrofitClient.instance.uploadImages(imageParts)
+                                                                val response =
+                                                                    RetrofitClient.instance.uploadImages(
+                                                                        imageParts
+                                                                    )
                                                                 withContext(Dispatchers.Main) {
                                                                     isUploading2DImages = false
-                                                                    resultUploading2DImages = if (response.isSuccessful) "Upload berhasil" else "Upload gagal: ${response.code()}"
+                                                                    resultUploading2DImages =
+                                                                        if (response.isSuccessful) "Upload berhasil" else "Upload gagal: ${response.code()}"
                                                                 }
                                                             } catch (e: Exception) {
                                                                 withContext(Dispatchers.Main) {
                                                                     isUploading2DImages = false
-                                                                    resultUploading2DImages = "Error: ${e.localizedMessage}"
+                                                                    resultUploading2DImages =
+                                                                        "Error: ${e.localizedMessage}"
                                                                 }
                                                             }
                                                         }
                                                     }
                                                 }
-                                            }else{
-                                                if (allImageFileListDb.isNotEmpty()) {
-                                                    val imageParts = getImageParts(allImageFileListDb = allImageFileListDb)
-                                                    CoroutineScope(Dispatchers.IO).launch {
-                                                        try {
-                                                            val response = RetrofitClient.instance.uploadImages(imageParts)
-                                                            withContext(Dispatchers.Main) {
-                                                                isUploading2DImages = false
-                                                                resultUploading2DImages = if (response.isSuccessful) "Upload berhasil" else "Upload gagal: ${response.code()}"
-                                                            }
-                                                        } catch (e: Exception) {
-                                                            withContext(Dispatchers.Main) {
-                                                                isUploading2DImages = false
-                                                                resultUploading2DImages = "Error: ${e.localizedMessage}"
-                                                            }
-                                                        }
-                                                    }
+                                            },
+                                        horizontalArrangement = Arrangement.Start,
+                                    ) {
+                                        Text("Peta 3D")
+                                        Icon(
+                                            imageVector = if (isButton3DExpandClicked) {
+                                                Icons.Filled.KeyboardArrowUp
+                                            } else {
+                                                Icons.Filled.KeyboardArrowDown
+                                            },
+                                            contentDescription = "expand button",
+                                            tint = Color(0xFF479AFF)
+                                        )
+                                    }
+                                    if (isButton3DExpandClicked) {
+                                        val brokenImage = painterResource(id = R.drawable.ic_broken_image)
+                                        val loadingImage = painterResource(id = R.drawable.loading_img)
+                                        Box() {
+                                            if (isUploading2DImages) {
+                                                Image(
+                                                    painter = loadingImage,
+                                                    contentDescription = "loading peta 3D",
+                                                    contentScale = ContentScale.Crop,
+                                                    modifier = Modifier.wrapContentSize()
+                                                )
+                                                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                                                    CircularProgressIndicator()
+                                                    Spacer(modifier = Modifier.height(12.dp))
+                                                    Text("Tunggu sebentar sedang memproses gambar...")
                                                 }
+                                            }else{
+                                                Image(
+                                                    painter = brokenImage, //ganti brokenImage dengan image respon dari server
+                                                    contentDescription = "peta 3D",
+                                                    contentScale = ContentScale.Crop,
+                                                    modifier = Modifier
+                                                        .size(200.dp)
+                                                )
                                             }
-                                        },
-                                    horizontalArrangement = Arrangement.Start,
-                                ) {
-                                    Text("Peta 3D")
-                                    Icon(
-                                        imageVector = if (isButton3DExpandClicked) {
-                                            Icons.Filled.KeyboardArrowUp
-                                        } else {
-                                            Icons.Filled.KeyboardArrowDown
-                                        },
-                                        contentDescription = "expand button",
-                                        tint = Color(0xFF479AFF)
-                                    )
+
+                                        }
+                                    }
                                 }
                             }
                         }
