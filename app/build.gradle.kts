8d862921756599f60c25e1c7791093152654f2a8
Jihand Aulia Sashiomarda <sashiomarda@gmail.com>
2025-05-22 16:21:03 +0700

    feat: update version 8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.sashiomarda.wifimapping"
         minSdk = 24
         targetSdk = 35
-        versionCode = 7
-        versionName = "7.0"
+        versionCode = 8
+        versionName = "8.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
