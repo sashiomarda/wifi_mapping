f337532cd92ac3282cf1b0e66ea4b555b623378a
Jihand Aulia Sashiomarda <sashiomarda@gmail.com>
2025-06-19 17:38:19 +0700

    fix: version 14 -> fix bug login using google account

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.sashiomarda.wifimapping"
         minSdk = 24
         targetSdk = 35
-        versionCode = 13
-        versionName = "13.0"
+        versionCode = 14
+        versionName = "14.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
